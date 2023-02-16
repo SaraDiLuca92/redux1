7ff748e10fafab56f8b25119fbb21eb811d356a1
SaraDiLuca92 <dl.sara@outlook.it>
2023-02-16 14:39:42 +0100

    mm

--- redux1/src/App.js
+++ redux1/src/App.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MainSearch from "./components/MainSearch";
-import CompanySearchResults from "./components/CompanySearchResults";
+import CompanySearchResults from "./components/MainSearch";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
